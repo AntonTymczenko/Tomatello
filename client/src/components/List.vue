47b7605cba61ba302f03477ed93ddc875690a7c0
Anton Tymczenko <tymczenko@protonmail.com>
2017-12-31 01:05:58 +0200

    use "f-vor" loop in unordered list

--- client/src/components/List.vue
+++ client/src/components/List.vue
@@ -2,15 +2,24 @@
 <div>
   <h3>Do today</h3>
   <ul>
-    <li>Walk dog</li>
-    <li>Go groceries</li>
-    <li>Do dishes</li>
+    <li v-for="todo in todos">
+      {{ todo.task }}
+    </li>
   </ul>
 </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      todos: [
+        {task: "Walk a dog"},
+        {task: "Go groceries"},
+        {task: "Do dishes"}
+      ]
+    }
+  }
 }
 </script>
 
