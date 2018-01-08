da5300f482cfc9fa4dd7649222d46718d3ad99b9
Anton Tymczenko <tymczenko@protonmail.com>
2018-01-08 17:25:09 +0200

    feat: Mark a list item as done or not done

--- client/src/components/List.vue
+++ client/src/components/List.vue
@@ -1,8 +1,14 @@
 <template>
 <div>
   <h3>Do today</h3>
-  <ul>
-    <li v-for="todo in todos">
+  <ul class="todo-list">
+    <li v-for="(todo, index) in todos">
+      <button @click="toggleDone(index)">
+        <v-icon
+          v-if="todo.done">check_box</v-icon>
+        <v-icon
+          v-else>check_box_outline_blank</v-icon>
+      </button>
       {{ todo.task }}
     </li>
   </ul>
@@ -14,15 +20,22 @@ export default {
   data () {
     return {
       todos: [
-        {task: 'Walk a dog'},
-        {task: 'Go groceries'},
-        {task: 'Do dishes'}
+        {task: 'Walk a dog', done: false},
+        {task: 'Go groceries', done: true},
+        {task: 'Do dishes', done: false}
       ]
+    }
+  },
+  methods: {
+    toggleDone (todoIndex) {
+      this.todos[todoIndex].done = !this.todos[todoIndex].done
     }
   }
 }
 </script>
 
 <style scoped>
-
+.todo-list li {
+  list-style: none;
+}
 </style>
