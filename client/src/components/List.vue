2d7df4b093d6cb0a88ed4bce772ec24dbee6ba8a
Anton Tymczenko <tymczenko@protonmail.com>
2018-01-08 18:13:34 +0200

    feat: Add new list item

--- client/src/components/List.vue
+++ client/src/components/List.vue
@@ -11,6 +11,15 @@
       </button>
       {{ todo.task }}
     </li>
+    <li class="new-list-item">
+      <button @click="toggleAddingItem"><v-icon>add</v-icon></button>
+      <input
+        ref="newItem"
+        v-model="newItemText"
+        v-if="addingItem"
+        @keyup.enter="addItem()"
+        @blur="addItem()">
+    </li>
   </ul>
 </div>
 </template>
@@ -23,12 +32,29 @@ export default {
         {task: 'Walk a dog', done: false},
         {task: 'Go groceries', done: true},
         {task: 'Do dishes', done: false}
-      ]
+      ],
+      addingItem: false,
+      newItemText: ''
     }
   },
   methods: {
     toggleDone (todoIndex) {
       this.todos[todoIndex].done = !this.todos[todoIndex].done
+    },
+    toggleAddingItem () {
+      this.addingItem = !this.addingItem
+      if (this.addingItem) {
+        this.$nextTick(() => {
+          this.$refs.newItem.focus()
+        })
+      }
+    },
+    addItem () {
+      if (this.newItemText !== '') {
+        this.todos.push({task: this.newItemText, done: false})
+      }
+      this.newItemText = ''
+      this.addingItem = false
     }
   }
 }
@@ -37,5 +63,8 @@ export default {
 <style scoped>
 .todo-list li {
   list-style: none;
+}
+.new-list-item input {
+  border-bottom: 1px solid black
 }
 </style>
