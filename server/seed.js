2752952e5bcfe5afc30b7f61313b8c6112fca04d
Anton Tymczenko <tymczenko@protonmail.com>
2018-01-13 00:17:33 +0200

    make Seed work

--- server/seed.js
+++ server/seed.js
@@ -22,3 +22,38 @@ const tasks = [{
   _list: list._id,
   task: 'Do dishes'
 }]
+
+const populateList = () => {
+  List.remove({})
+    .then(() => {
+      return new List(list).save()
+    })
+    .then(listSaved => {
+      populateTasks(listSaved)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+const populateTasks = list => {
+  Task.remove({})
+    .then(() => {
+      const tasksToReturn = []
+      tasks.map(task => {
+        tasksToReturn.push(new Task(task).save())
+      })
+      return Promise.all(tasksToReturn)
+    })
+    .then(() => {
+      tasks.map(task => {
+        list.tasks.push(task._id)
+      })
+      list.save()
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+populateList()
