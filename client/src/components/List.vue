76ae24b7085e54a9a73dabbe7fb1b8c757088abb
Anton Tymczenko <tymczenko@protonmail.com>
2018-01-13 23:30:32 +0200

    fix: Rename Todos in list into Tasks to be semantically better.

--- client/src/components/List.vue
+++ client/src/components/List.vue
@@ -2,14 +2,14 @@
 <div>
   <h3>Do today</h3>
   <ul class="todo-list">
-    <li v-for="(todo, index) in todos">
+    <li v-for="(task, index) in tasks">
       <button @click="toggleDone(index)">
         <v-icon
-          v-if="todo.done">check_box</v-icon>
+          v-if="task.done">check_box</v-icon>
         <v-icon
           v-else>check_box_outline_blank</v-icon>
       </button>
-      {{ todo.task }}
+      {{ task.task }}
     </li>
     <li class="new-list-item">
       <button @click="toggleAddingItem"><v-icon>add</v-icon></button>
@@ -28,18 +28,14 @@
 export default {
   data () {
     return {
-      todos: [
-        {task: 'Walk a dog', done: false},
-        {task: 'Go groceries', done: true},
-        {task: 'Do dishes', done: false}
-      ],
+      tasks: [],
       addingItem: false,
       newItemText: ''
     }
   },
   methods: {
-    toggleDone (todoIndex) {
-      this.todos[todoIndex].done = !this.todos[todoIndex].done
+    toggleDone (taskIndex) {
+      this.tasks[taskIndex].done = !this.tasks[taskIndex].done
     },
     toggleAddingItem () {
       this.addingItem = !this.addingItem
@@ -51,7 +47,7 @@ export default {
     },
     addItem () {
       if (this.newItemText !== '') {
-        this.todos.push({task: this.newItemText, done: false})
+        this.tasks.push({task: this.newItemText, done: false})
       }
       this.newItemText = ''
       this.addingItem = false
