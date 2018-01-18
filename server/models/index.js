a755255cb380c7e50ee7736af727576aa68b5986
Anton Tymczenko <tymczenko@protonmail.com>
2018-01-18 21:45:26 +0200

    fix: improve auto search of Models. Only files starting wiht capital letter are allowed

--- server/models/index.js
+++ server/models/index.js
@@ -1,10 +1,11 @@
 const fs = require('fs'),
   path = require('path'),
+  pattern = /^[A-Z].*\.js$/
   models = {}
 
 fs
   .readdirSync(__dirname)
-  .filter(file => file !== 'index.js')
+  .filter(file => (file !== 'index.js') && (pattern.test(file)))
   .forEach(file => {
     models[path.basename(file, path.extname(file))] = require('./' + file)
   })
