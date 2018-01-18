014f5f1f7f139beaa0a173f4b616e9d365643e70
Anton Tymczenko <tymczenko@protonmail.com>
2018-01-18 21:43:50 +0200

    add users index.js file to automatically fetch all the .json files from folder

---
+++ server/seed/users/index.js
@@ -0,0 +1,13 @@
+const fs = require('fs'),
+  path = require('path'),
+  pattern = /\.json$/
+  users = []
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => (file !== 'index.js') && (pattern.test(file)))
+  .forEach(file => {
+    users.push(require('./' + file))
+  })
+
+module.exports = users
