c15414879951737dc342ea53732c6490bcefa917
Anton Tymczenko <tymczenko@protonmail.com>
2018-01-18 21:50:39 +0200

    fix: List model is related to User and Board

--- server/models/List.js
+++ server/models/List.js
@@ -1,6 +1,16 @@
 const mongoose = require('mongoose')
 
 const ListSchema = new mongoose.Schema({
+  _user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  _board: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Board',
+    required: true
+  },
   listName: {
     type: String,
     required: false,
