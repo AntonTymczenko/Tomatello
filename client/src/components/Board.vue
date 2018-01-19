0fbfc0d46a90022eb44026df534b2c40b2f65213
Anton Tymczenko <tymczenko@protonmail.com>
2018-01-19 22:07:17 +0200

    make Board component. it fetches first board from DB, not specific

---
+++ client/src/components/Board.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+  <h2>{{ boardName }}</h2>
+  <div v-for="listId in lists">
+    <app-list :listId="listId"></app-list>
+  </div>
+</div>
+</template>
+
+<script>
+import List from './List.vue'
+import axios from 'axios'
+
+export default {
+  components: {
+    appList: List
+  },
+  data: () => ({
+    boardName: '',
+    lists: [],
+    user: ''
+  }),
+  created () {
+    axios.get(`/board/1`)
+      .then(res => {
+        this.boardName = res.data.boardName
+        this.lists = res.data.lists
+        this.user = res.data._user
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+}
+</script>
+
+<style scoped>
+</style>
