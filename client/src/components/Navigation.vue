ae0c479f39c7259c0cda63e87f6899d93594103d
Anton Tymczenko <tymczenko@protonmail.com>
2018-01-21 00:33:08 +0200

    add Userpic and Username to navigation

--- client/src/components/Navigation.vue
+++ client/src/components/Navigation.vue
@@ -1,27 +1,20 @@
 <template>
 <v-list dense>
-  <v-list-tile @click="">
-    <v-list-tile-action>
-      <v-icon>home</v-icon>
-    </v-list-tile-action>
+  <v-list-tile avatar>
+    <v-list-tile-avatar>
+      <img src="https://randomuser.me/api/portraits/men/85.jpg" />
+    </v-list-tile-avatar>
     <v-list-tile-content>
-      <v-list-tile-title>Home</v-list-tile-title>
+      <v-list-tile-title>John Doe</v-list-tile-title>
     </v-list-tile-content>
   </v-list-tile>
-  <v-list-tile @click="">
+  <v-divider></v-divider>
+  <v-list-tile v-for="item in items" :key="item.title" @click="">
     <v-list-tile-action>
-      <v-icon>contact_mail</v-icon>
+      <v-icon>{{ item.icon }}</v-icon>
     </v-list-tile-action>
     <v-list-tile-content>
-      <v-list-tile-title>Contact</v-list-tile-title>
-    </v-list-tile-content>
-  </v-list-tile>
-  <v-list-tile @click="resetDatabase()">
-    <v-list-tile-action>
-      <v-icon>settings_backup_restore</v-icon>
-    </v-list-tile-action>
-    <v-list-tile-content>
-      <v-list-tile-title>Reset Database</v-list-tile-title>
+      <v-list-tile-title>{{ item.title }}</v-list-tile-title>
     </v-list-tile-content>
   </v-list-tile>
 </v-list>
@@ -31,7 +24,17 @@
 import axios from 'axios'
 
 export default {
+  data: () => ({
+    items: [
+      {title: 'Home', icon: 'home'},
+      {title: 'Contacts', icon: 'contact_mail'},
+      {title: 'Reset Database', icon: 'settings_backup_restore', click: 'poop()'}
+    ]
+  }),
   methods: {
+    poop () {
+      alert('poop')
+    },
     resetDatabase () {
       axios.delete('/reset')
         .then(res => {
