f874ebe5d04d1e8eea78add2e705b91136d6b017
Anton Tymczenko <tymczenko@protonmail.com>
2018-01-21 01:13:28 +0200

    separate development Navigation items from main list

--- client/src/components/Navigation.vue
+++ client/src/components/Navigation.vue
@@ -17,6 +17,21 @@
       <v-list-tile-title>{{ item.title }}</v-list-tile-title>
     </v-list-tile-content>
   </v-list-tile>
+  <div v-if="devItems">
+    <v-divider></v-divider>
+    <v-list-tile
+      v-for="(item, index) in devItems"
+      :key="item.title"
+      @click="devTools(index)">
+      <v-list-tile-action>
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-list-tile-action>
+      <v-list-tile-content>
+        <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+      </v-list-tile-content>
+    </v-list-tile>
+  </div>
+  <v-list-tile></v-list-tile>
 </v-list>
 </template>
 
@@ -28,20 +43,25 @@ export default {
     items: [
       {title: 'Home', icon: 'home', link: '/'},
       {title: 'Contacts', icon: 'contact_mail', link: '/profile'}
+    ],
+    devItems: [
+      {title: 'Reset Database',
+        icon: 'settings_backup_restore',
+        click: () => {
+          axios.delete('/reset')
+            .then(res => {
+              location.reload(true)
+            })
+            .catch(err => {
+              console.log(err)
+            })
+        }
+      }
     ]
   }),
   methods: {
-    poop () {
-      alert('poop')
-    },
-    resetDatabase () {
-      axios.delete('/reset')
-        .then(res => {
-          location.reload(true)
-        })
-        .catch(err => {
-          console.log(err)
-        })
+    devTools (index) {
+      this.devItems[index].click()
     }
   }
 }
