ef3a6c42923fe00ed8c9bd9778403eb6b82183de
Anton Tymczenko <tymczenko@protonmail.com>
2018-01-21 02:10:30 +0200

    Homepage component shows the first Board of current User

--- client/src/components/Homepage.vue
+++ client/src/components/Homepage.vue
@@ -1,44 +1,32 @@
 <template>
-<v-app id="inspire">
-  <v-navigation-drawer fixed v-model="drawer" app >
-    <app-navigation/>
-  </v-navigation-drawer>
-  <v-toolbar color="indigo" dark fixed app>
-    <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-    <v-toolbar-title>Turbo Trello</v-toolbar-title>
-  </v-toolbar>
-  <v-content>
-    <v-container fluid fill-height>
-      <v-layout justify-center align-center >
-        <app-board></app-board>
-      </v-layout>
-    </v-container>
-  </v-content>
-  <v-footer color="indigo" app>
-    <span class="white--text">&copy; 2017</span>
-  </v-footer>
-</v-app>
+<div>
+  <app-board
+    :board="board"
+  ></app-board>
+</div>
 </template>
-
 <script>
-import Navigation from './Navigation.vue'
 import Board from './Board.vue'
+import axios from 'axios'
 
 export default {
-  components: {
-    appBoard: Board,
-    appNavigation: Navigation
-  },
-  data () {
-    return {
-      drawer: null
-    }
-  },
   props: {
-    source: String
+    user: {required: true}
+  },
+  data: () => ({
+    board: null
+  }),
+  components: {
+    appBoard: Board
+  },
+  created () {
+    axios.get(`/board/${this.user.boards[0]}`)
+      .then(res => {
+        this.board = res.data
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 }
 </script>
-
-<style scoped>
-</style>
