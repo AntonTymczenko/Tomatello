d723c3f83a5e766bf95a273399570962cf359d1a
Anton Tymczenko <tymczenko@protonmail.com>
2018-01-23 01:26:26 +0200

    get Board by request instead of Props

--- client/src/components/Board.vue
+++ client/src/components/Board.vue
@@ -1,5 +1,5 @@
 <template>
-<div>
+<div v-if="board">
   <h2>{{ board.boardName }}</h2>
   <div v-for="listId in board.lists">
     <app-list
@@ -10,13 +10,31 @@
 </template>
 
 <script>
+import axios from 'axios'
 import List from './List.vue'
 
 export default {
-  props: {
-    board: {
-      required: true
+  props: ['id'],
+  data: () => ({
+    board: null
+  }),
+  computed: {
+    user () {
+      return this.$store.state.user
     }
+  },
+  created () {
+    axios.get(`/board/${this.id}`)
+      .then(res => {
+        if (!res) {
+          throw new Error()
+        }
+        this.board = res.data
+      })
+      .catch(err => {
+        console.log(err)
+        this.$router.push({name: 'Homepage'})
+      })
   },
   components: {
     appList: List
