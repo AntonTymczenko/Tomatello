ecd2a5bfc7dc099b40ecc7dcc81e8a9e55f76090
Anton Tymczenko <tymczenko@protonmail.com>
2018-01-24 02:27:52 +0200

    add new component BoardControls for Dashboard with control buttons

---
+++ client/src/components/Dashboard/BoardControls.vue
@@ -0,0 +1,39 @@
+<template>
+<v-speed-dial
+  v-model="fab"
+  direction="left"
+  transition="slide-x-reverse-transition"
+>
+  <v-btn
+    slot="activator"
+    color="transparent"
+    light fab hover small
+    v-model="fab"
+  >
+    <v-icon>more_horiz</v-icon>
+    <v-icon>close</v-icon>
+  </v-btn>
+  <v-btn
+    @click.stop="editBoard(index)"
+    fab dark small color="green"
+  >
+    <v-icon>edit</v-icon>
+  </v-btn>
+  <v-btn
+    @click.stop="deleteBoard(index)"
+    fab dark small color="red"
+  >
+    <v-icon>delete</v-icon>
+  </v-btn>
+</v-speed-dial>
+</template>
+
+
+<script>
+export default {
+  props: ['index', 'deleteBoard', 'editBoard'],
+  data: () => ({
+    fab: false
+  })
+}
+</script>
