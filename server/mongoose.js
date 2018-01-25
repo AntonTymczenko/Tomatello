66551160c5a30efaa7149f9b947d576a4d5474b1
Anton Tymczenko <tymczenko@protonmail.com>
2018-01-25 16:24:51 +0200

    improve mongoose connection check for error

--- server/mongoose.js
+++ server/mongoose.js
@@ -7,5 +7,12 @@ mongoose.Promise = global.Promise
 module.exports = dburl => {
   mongoose.connect(dburl, options)
     .then(() => console.log(`Connected to database ${dburl}`))
-    .catch(err => console.log(err))
+    .catch(err => {
+      if (err.message.indexOf('ECONNREFUSED') !== -1) {
+        console.error('Error: The server was not able to reach MongoDB')
+        process.exit(1)
+      } else {
+        throw err
+      }
+    })
 }
