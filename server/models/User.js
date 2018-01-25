735976c943fed2939abcc3f0b5a28bb355e66b94
Anton Tymczenko <tymczenko@protonmail.com>
2018-01-25 18:34:49 +0200

    save hashed password

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
   login: {
@@ -28,6 +29,21 @@ const UserSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Board'
   }]
+})
+
+UserSchema.pre('save', function (next) {
+  let user = this
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) =>{
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash
+        next()
+      })
+    })
+  } else {
+    next()
+  }
 })
 
 module.exports = mongoose.model('User', UserSchema)
