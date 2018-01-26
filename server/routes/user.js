1d0d27e9069f7ea45e23aa5093d2e02363579827
Anton Tymczenko <tymczenko@protonmail.com>
2018-01-26 18:28:48 +0200

    feat: add login by Auth token feature

--- server/routes/user.js
+++ server/routes/user.js
@@ -45,9 +45,11 @@ module.exports = (prefix, router) => {
       })
   })
 
-  // login by credentials
+  // login
   router.post('/login', (req, res) => {
     const {login, password} = req.body
+    if (login && password) {
+      // login by credentials
       User.findByCredentials(login, password)
         .then(async user => {
           const token = await user.giveAuthToken()
@@ -61,6 +63,23 @@ module.exports = (prefix, router) => {
         .catch(err => {
           res.status(403).send('Wrong credentials')
         })
+    } else {
+      const token = req.header('x-auth')
+      if (token) {
+        // login by Auth Token
+        User.findByToken(token)
+          .then(user => {
+            if (!user) {
+              res.status(401).send('Wrong Auth Token')
+            } else {
+              res.status(200).send(user)
+            }
+          })
+          .catch(err => worstScenario(err, res))
+      } else {
+        res.status(403).send('Access denied')
+      }
+    }
   })
 
   // user UPDATE:
