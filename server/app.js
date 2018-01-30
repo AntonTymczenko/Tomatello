c64404e81324f6a9212ef5e15c044e9f5d550e34
Anton Tymczenko <tymczenko@protonmail.com>
2018-01-31 00:44:09 +0200

    chore: always use `dotenv`

--- server/app.js
+++ server/app.js
@@ -1,18 +1,18 @@
 const path = require('path')
 const express = require('express')
 const app = express()
-const NODE_ENV = process.env.NODE_ENV || 'development'
+
+require('dotenv').config()
+const {NODE_ENV, PORT, URL, MONGODB_URI} = process.env
 
 // application-level middleware:
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 if (NODE_ENV === 'development') {
-  require('dotenv').config()
   const cors = require('cors')
   app.use(cors({ exposedHeaders: ['x-auth'] }))
 }
 app.use(express.static(path.resolve(__dirname, '../client/dist')))
-const {PORT, URL, MONGODB_URI} = process.env
 
 // connect to database:
 require('./mongoose')(MONGODB_URI)
