2d6bb4b10c2a2d03f41f3bc8cb0aca509fe701b6
Anton Tymczenko <tymczenko@protonmail.com>
2018-01-31 00:07:59 +0200

    fix: if NODE_ENV is not set, let it be `dev` by default

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
 const app = express()
-const {NODE_ENV} = process.env
+const NODE_ENV = process.env.NODE_ENV || 'development'
 
 // application-level middleware:
 const bodyParser = require('body-parser')
