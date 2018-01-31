17327d51f5849e6187493170c50b9cf5e95add79
Anton Tymczenko <tymczenko@protonmail.com>
2018-01-31 23:47:36 +0200

    feat: add new middleware functions
    authenticated: make sure the `x-auth` header has a registered token)
    authorized: check if authenticated, then check if requested route is
    about the same user who's requesting
    Also put `hasAccessToBoard` outside the `exports`

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,11 +1,50 @@
 const {User, Board, List, Task} = require('../models')
+const {ObjectID} = require('mongodb')
 
-module.exports = {
-  hasAccessToBoard (req, res, next) {
-    if (true) {
+const error = [
+  {},
+  {error: 'Not authorized for this action'},
+  {},
+  {error: 'Access denied'}
+]
+
+const authenticated = (req, res, next) => {
+  const token = req.header('x-auth')
+  User.findByToken(token)
+    .then(user => {
+      if (!user) {
+        return Promise.reject(403)
+      }
+      req.user = user
+      next()
+    })
+    .catch(err => {
+      res.status(403).send(error[3])
+    })
+}
+
+const authorizedToUpdateUser = (req, res, next) => {
+  authenticated(req, res, () => {
+    if (ObjectID.isValid(req.params.id)
+      && ObjectID.isValid(req.user._id)
+      && req.params.id.toString() === req.user._id.toString()
+    ){
       next()
     } else {
-      res.status(403).send({error: 'Access denied'})
+      res.status(401).send(error[1])
     }
+  })
+}
+
+const hasAccessToBoard = (req, res, next) => {
+  if (true) {
+    next()
+  } else {
+    res.status(401).send(error[1])
   }
+}
+
+module.exports = {
+  authorizedToUpdateUser,
+  hasAccessToBoard
 }
