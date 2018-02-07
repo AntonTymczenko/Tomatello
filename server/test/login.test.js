c9a4559a88cd7150e988e87de591df9f7e10bb17
Anton Tymczenko <tymczenko@protonmail.com>
2018-02-08 01:54:45 +0200

    refactor: don't populate all `seed` users

--- server/test/login.test.js
+++ server/test/login.test.js
@@ -3,24 +3,20 @@ const app = require('../app'),
   chaiHttp = require('chai-http'),
   {expect} = require('chai'),
   should = chai.should(),
-  {resetAllCollections, populateUsers, users} = require('../seed'),
   errors = require('../errors.json')
+  {resetAllCollections, users} = require('../seed'),
 
 chai.use(chaiHttp)
 
 before(resetAllCollections)
 
 describe('Sign-up route in API', () => {
-  before(populateUsers)
   const path = '/signup'
 
   it('should register a proper user', done => {
     chai.request(app)
-      .post('/signup')
-      .send({
-        login: 'vasa',
-        password: 'pwd'
-      })
+      .post(path)
+      .send(users[0])
       .end((err, res) => {
         res.should.have.status(200)
         done()
